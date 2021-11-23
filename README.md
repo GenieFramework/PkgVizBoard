80075f9cc7a62feccc164e693247225234a6f89e
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2021-11-23 14:25:35 +0530

    Downloading and saving to CSV finished

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 #### Runing Tasks
 
 ```julia
+julia> using Genie.Toolbox
 julia> Toolbox.printtasks(PkgVizBoard)
 julia> PkgVizBoard.S3DownloadTask.runtask()
 ```
--- tasks/S3DownloadTask.jl
+++ tasks/S3DownloadTask.jl
@@ -1,8 +1,11 @@
 module S3DownloadTask
 
-using GZip
+using Genie
+using GZip, CSV, CSVFiles, DataFrames
 
-# Get links to all the files in the S3 bucket
+"""
+Get links to all the files in the S3 bucket
+"""
 function s3_urls()
   s3_url = "https://julialang-logs.s3.amazonaws.com/public_outputs/current/"
   rollup_names = ["client_types", "julia_systems", "julia_versions", "resource_types", "package_requests"]
@@ -19,8 +22,10 @@ function s3_urls()
   return roll_up_urls
 end
 
-# download giz files periodically from urls
-# Place in root/db/data/
+"""
+download giz files periodically from urls
+Place in root/db/data/
+"""
 function download_s3_urls(urls)
   for i in 1:size(urls)[1]
     store = joinpath(pwd(), "db/data")
@@ -31,10 +36,25 @@ function download_s3_urls(urls)
 end
 
 
-# unzip, delete original file
-# Place in root/db/data/
-fuction unzip()
+"""
+unzip, delete original file
+Place in root/db/seeds/
+"""
+function unzip()
+  printstyled("\nUnzipping Files 🤐 🤐 🤐 ... \n"; color = :blue)
+  
+  data_path = "db/data" #"$(Main.UserApp)/db/data" 
 
+  gz_data = readdir(data_path)
+
+  for file in gz_data
+    f_in = GZip.open("db/data/$(file)", "rb")
+    stream = read(f_in)
+    write("db/seeds/$(chop(file, tail=3))", stream)
+    printstyled("unzipped $(file) | ✅  \n"; color = :magenta)
+    close(f_in)
+  end
+  
 end
 
 """
@@ -43,10 +63,13 @@ Downloads S3 files to local disk perioidically.
 function runtask()
   urls = s3_urls()
 
+  # downloading data to db/data
   download_s3_urls(urls)
 
-  
   printstyled("Finished task 🚀 🚀 🚀"; color = :yellow)
+
+  #unzipping csv files and saving to db/seeds
+  unzip()
 end
 
 end
