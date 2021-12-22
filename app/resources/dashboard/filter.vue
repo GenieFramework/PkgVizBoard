5bea71bb43e0da270a4ba0c5b209ec5136db8f14
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2021-12-22 19:20:38 +0530

    auto complete

---
+++ app/resources/dashboard/filter.vue
@@ -0,0 +1,23 @@
+<script>
+const stringOptions = [
+  'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+]
+
+export default {
+  data () {
+    return {
+      model: null,
+      options: stringOptions
+    }
+  },
+
+  methods: {
+    filterFn (val, update, abort) {
+      update(() => {
+        const needle = val.toLowerCase()
+        this.options = stringOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
+      })
+    }
+  }
+}
+</script>
