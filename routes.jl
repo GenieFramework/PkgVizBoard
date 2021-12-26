595031bc3f251b7f59bb3efb7bc0961e98965d46
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2021-12-27 03:16:58 +0530

    removing comment

--- routes.jl
+++ routes.jl
@@ -1,21 +1,4 @@
 using DashboardController, StatsController
 using Genie, Stipple, StippleUI, StipplePlotly
 
-# const assets_config = Genie.Assets.AssetsConfig(package = "PkgVizBoard")
-
-#=== config ==#
-
-if Genie.Configuration.isprod()
-  for m in [Genie, Stipple, StippleUI, StipplePlotly]
-    m.assets_config.host = "https://cdn.statically.io/gh/GenieFramework"
-  end
-end
-
-#== server ==#
-
 route("/", DashboardController.dashboard)
-
-route("/api/v1/regions", StatsController.API.V1.regions, method = GET)
-route("/api/v1/packages", StatsController.API.V1.packages, method = GET)
-[route("/api/v1/stats", StatsController.API.V1.search, method = m) for m in [GET, POST]]
-route("/api/v1/badge/:packages", StatsController.API.V1.badge, method = GET)
