5ccc31c60458ceb85bd46064d14683c9f6650e6f
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-01-13 00:47:35 +0530

    Cleanup updated cache logic

--- app/resources/packages/PackagesController.jl
+++ app/resources/packages/PackagesController.jl
@@ -4,7 +4,7 @@ using SearchLight, Genie.Renderers.Js, Genie.Cache
 using Packages, Dashboard
 
 function packagenames()
-  withcache("pkgnames", 24 * 60 * 60) do # 24 hours
+  withcache(:packagenames, 24 * 60 * 60) do # 24 hours
     pkgnames = [pkgname.name for pkgname in all(Package)]
     "const packageList = $pkgnames" |> js
   end
