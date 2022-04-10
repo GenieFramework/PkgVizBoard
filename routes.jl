ba4d4f332e6329f6dbe49275e879b8e43b80acfc
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-04-10 11:53:05 +0530

    WIP api docs for regions

--- routes.jl
+++ routes.jl
@@ -1,5 +1,6 @@
 using DashboardController, StatsController
 using Genie, Stipple, StippleUI, StipplePlotly
+using SwagUI, SwaggerMarkdown
 
 #=== config ==#
 
@@ -13,8 +14,29 @@ end
 
 route("/", DashboardController.dashboard)
 
+@swagger """
+/api/v1/regions:
+  get:
+    description: Get regions
+    responses:
+      '200':
+        description: Returns a JSON object with all the regions
+"""
 route("/api/v1/regions", StatsController.API.V1.regions, method = GET)
+
+
 route("/api/v1/packages", StatsController.API.V1.packages, method = GET)
 [route("/api/v1/stats", StatsController.API.V1.search, method = m) for m in [GET, POST]]
 route("/api/v1/badge/:packages", StatsController.API.V1.badge, method = GET)
 route("/api/v1/badge/:packages/:options", StatsController.API.V1.badge, method = GET, named = :get_api_v1_badge_packages_options)
+
+# build a swagger document from markdown
+info = Dict{String, Any}()
+info["title"] = "PkgVizBoard API"
+info["version"] = "1.0.5"
+openApi = OpenAPI("2.0", info)
+swagger_document = build(openApi)
+
+route("/docs") do 
+    render_swagger(swagger_document)
+end
