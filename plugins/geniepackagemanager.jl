15178c0c0aa8378c16db2228b116be0b82c0c850
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-09-28 15:03:05 +0530

    Updated Plugin

--- plugins/geniepackagemanager.jl
+++ plugins/geniepackagemanager.jl
@@ -3,8 +3,11 @@ using Genie.Requests
 using Genie.Renderer.Json
 using GeniePackageManager
 
+
 route("/geniepackagemanager", GeniePackageManager.list_packages)
+route("/geniepackagemanager/packages", GeniePackageManager.get_packages)
 
-route("/geniepackagemanager/add", GeniePackageManager.add, method = POST)
-
-route("/geniepackagemanager/:package::String/remove", GeniePackageManager.remove_package, method = POST)
+# REST ENDPOINTS
+route("/geniepackagemanager/api/v1/:package::String/add", GeniePackageManager.API.V1.add, method = POST)
+route("geniepackagemanager/api/v1/:package::String/remove", GeniePackageManager.API.V1.remove_package, method = POST)
+route("/geniepackagemanager/api/v1/:package::String/update", GeniePackageManager.API.V1.update_package, method = POST)
